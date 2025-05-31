2ccac142d68549ee135965645ad84008642f4a8a
I-Min Hsieh Anderson <ee7788965000@gmail.com>
2025-05-31 21:14:01 +0200

    Made tables of professionals and institutions

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("professionals/", views.professionals_view, name="professionals_list"),
+    path("institutions/", views.institutions_view, name="institutions_list")
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from post.models import Post
+from profile_page.models import ProfessionalProfile, InstitutionProfile
+from django.db.models.functions import Coalesce
+from django.db.models import Sum, Value, Count
 
 def home(request):
     posts = Post.objects.order_by("-time_posted")[:5]
@@ -9,3 +12,33 @@ def home(request):
         "posts" : posts
     }
     return render(request, "core/home.html", context=context)
+
+
+def professionals_view(request):
+    # get all professionals (also including those with 0 ratings for their answers)
+    # and rank them according to amount of ratings. For professionals with the same
+    # amount of total ratings, order by their total number of answers.
+    professionals = ProfessionalProfile.objects.annotate(
+        total_ratings=Coalesce(Sum("answers__ratings__value"), Value(0)),
+        answer_count=Count("answers")
+    ).order_by("-total_ratings", "-answer_count")
+
+    context = {
+        "title" : "Professionals",
+        "professionals" : professionals
+    }
+    return render(request, "core/professionals.html", context=context)
+
+
+def institutions_view(request):
+    institutions = InstitutionProfile.objects.annotate(
+        total_ratings=Coalesce(Sum("professionals__answers__ratings__value"), Value(0)),
+        answer_count=Count("professionals__answers")
+    ).order_by("-total_ratings", "-answer_count")
+
+    context = {
+        "title" : "Professionals",
+        "institutions" : institutions
+    }
+    return render(request, "core/institutions.html", context=context)
+
