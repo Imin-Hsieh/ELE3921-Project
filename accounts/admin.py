e0921475048c4b527519fa8a1ab201dca2de2eae
I-Min Hsieh Anderson <ee7788965000@gmail.com>
2025-05-31 21:45:02 +0200

    small admin change

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,8 +7,8 @@ class CustomAdmin(UserAdmin):
     add_form = UserCreationForm
     form = UserChangeForm
     model = User
-    list_display = ["username", "email", "account_type"]
+    list_display = ["username", "first_name", "last_name", "account_type"]
     fieldsets = UserAdmin.fieldsets + ((None, {"fields" : ("account_type",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields" : ("account_type",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields" : ("first_name", "last_name", "account_type",)}),)
 
 admin.site.register(User, CustomAdmin)
